3171d2b10d3e98b7dad546c74e9428c5db153912
Joseph <thomas1940j@gmail.com>
2026-05-19 08:28:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -902,5 +902,5 @@ def export_report():
 # RUN APP
 # ====================================================
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
